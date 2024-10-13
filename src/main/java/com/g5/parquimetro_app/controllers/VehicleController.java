a842be0588826cca1235c3076cbb664daf0be99d
Kennia-Sobrinho <kennia.a7x@hotmail.com>
2024-10-13 11:41:29 -0300

    feat: add documentation

--- src/main/java/com/g5/parquimetro_app/controllers/VehicleController.java
+++ src/main/java/com/g5/parquimetro_app/controllers/VehicleController.java
@@ -6,19 +6,25 @@ import org.springframework.web.bind.annotation.*;
 import com.g5.parquimetro_app.models.Vehicle;
 import com.g5.parquimetro_app.services.VehicleService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping(value = "/")
+@Tag(name = "Vehicle", description = "Vehicle API") 
 public class VehicleController {
 
     @Autowired
     private VehicleService vehicleService;
 
     @PostMapping("/start")
+    @Operation(summary = "Start parking", description = "Start parking for a vehicle")
     public Vehicle startParking(@RequestParam String plateNumber){
         return vehicleService.startParking(plateNumber);
     }
 
     @PostMapping("/end")
+    @Operation(summary = "End parking", description = "End parking for a vehicle")
     public Vehicle endParking(@RequestParam String plateNumber){
         return vehicleService.endParking(plateNumber);
     }
